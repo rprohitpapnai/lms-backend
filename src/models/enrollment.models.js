744d06343291cdde2690193b5f65968f38913809
rprohitpapnai <rprohitpapnai@gmail.com>
2026-05-02 23:14:11 +0530

    the enrollment model has been created

--- src/models/enrollment.models.js
+++ src/models/enrollment.models.js
@@ -1,5 +1,59 @@
 import mongoose, {Schema } from "mongoose";
-const enrollmentSchema = new Schema ({},{ timestamps:true })
+const enrollmentSchema = new Schema ({\
+    studentId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    courseId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Course",
+        required:true
+    },
+    teacherId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+    organisationId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Organisation",
+        required:true
+    },
+    totalClasses:{
+        type:Number,
+        required:true,
+    },
+    remainingClasses:{
+        type:Number,
+        required:true
+    },
+    planType:{
+        type:String,
+        enum:["one on one", "group","monthly","custom "],
+        default:"one on one",
+        required:true
+    },
+    status:{
+        type:String,
+        enum:["active","completed","cancelled","payment pending"],
+        required:true,
+        default:"pending"
+    },
+    cost:{
+        type:Number,
+        
+    },
+    totalAmount:{
+        type:Number,
+        required:true
+    },
+    paidAmount:{
+        type:Number,
+        required:true
+    },
+    
+},{ timestamps:true })
 
 
 export const Enrollment = mongoose.model("Enrollment", enrollmentSchema)

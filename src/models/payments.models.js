0da773ec1acec116f7d1e9fe1298c405d6282c2c
rprohitpapnai <rprohitpapnai@gmail.com>
2026-05-06 18:44:56 +0530

    the payment schema has been created

--- src/models/payments.models.js
+++ src/models/payments.models.js
@@ -1,9 +1,43 @@
 import mongoose, {Schema } from "mongoose";
 
 const paymentSchema = new Schema ({
+studentId:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"User",
+    required:true
+},
+enrollmentId:{
+    type:mongoose.Schema.Types.ObjectId,
+    ref:"Enrollment",
+    required:true
+},
+amount:{
+    type:Number,
+    required:true
+},
+currency:{
+    type:String,
+    default:"USD"
+},
+status:{
+    type:String,
+    enum:["pending","active","completed","cancelled"],
+    required:true,
+    default:"pending"
+},
+transactionId:{
+    type:String,
+    required:true
+},
+paymentDate:{
+    type:Date,
+    default:Date.now
+}
 
-    
 },{ timestamps:true })
+
+paymentSchema.index({ studentId: 1 });
+paymentSchema.index({ enrollmentId: 1 });
 
 
 
